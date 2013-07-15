d5377999ff7f080a58b60ceb39d2081284a96881
Paul Bouchier <paul.bouchier@gmail.com>
2013-07-15 18:38:51 -0500

    Imported upstream version '0.5.1' of 'upstream'

--- rosserial_python/nodes/serial_node.py
+++ rosserial_python/nodes/serial_node.py
@@ -43,13 +43,18 @@ import sys
    
 if __name__=="__main__":
 
+    rospy.init_node("serial_node")
+    rospy.loginfo("ROS Serial Python Node")
+
     port_name = rospy.get_param('~port','/dev/ttyUSB0')
     baud = int(rospy.get_param('~baud','57600'))
-    tcp_portnum = int(rospy.get_param('/rosserial_embeddedlinux/tcp_port', '11411'))
-    fork_server = rospy.get_param('/rosserial_embeddedlinux/fork_server', True)
 
+    # TODO: should these really be global?
+    tcp_portnum = int(rospy.get_param('/rosserial_embeddedlinux/tcp_port', '11411'))
+    fork_server = rospy.get_param('/rosserial_embeddedlinux/fork_server', False)
+
+    # TODO: do we really want command line params in addition to parameter server params?
     sys.argv = rospy.myargv(argv=sys.argv)
-    
     if len(sys.argv) == 2 :
         port_name  = sys.argv[1]
     if len(sys.argv) == 3 :
@@ -71,8 +76,6 @@ if __name__=="__main__":
             rospy.loginfo("All done")
 
     else :          # Use serial port 
-        rospy.init_node("serial_node")
-        rospy.loginfo("ROS Serial Python Node")
         rospy.loginfo("Connecting to %s at %d baud" % (port_name,baud) )
         client = SerialClient(port_name, baud)
         try:
